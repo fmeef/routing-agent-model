fef6d0d71aa0e1ffe036b2358e4beae17337282f
foo <foo>
2018-12-03 13:33:42 -0600

    Move hoominworld init to top (nonfunction change)

--- realhoomin/model.py
+++ realhoomin/model.py
@@ -30,84 +30,6 @@ class HoominWorld(Model):
     verbose = False
     description = "A model of foot traffic and radio communication in an urban environment"
 
-    def roadplace_grid(self):
-        for h in range(self.height):
-            if h % self.gridspacing is 0:
-                for w in range(self.width):
-                    road = Road(self.next_id(), (w,h), self)
-                    self.grid.place_agent(road, (w,h))
-
-        for w in range(self.width):
-            if w % self.gridspacing is 0:
-                for h in range(self.height):
-                    road = Road(self.next_id(), (w,h), self)
-                    self.grid.place_agent(road, (w,h))
-
-
-
-
-    def roadplace_random(self, direction=0):
-
-        if direction is HoominWorld.STRAIGHT:
-            True
-        elif direction is HoominWorld.LEFT:
-            self.roaddir = np.array((-1 * self.roaddir[1], self.roaddir[0]))
-        elif direction is HoominWorld.RIGHT:
-            self.roaddir = np.array((self.roaddir[1], -1 * self.roaddir[0]))
-        else:
-            self.roaddir = np.array((0,0))
-            print("bad bad bad")
-        print("placing road, direction ", direction, " coord: ", self.roadcurrentcoord)
-
-
-        newcoord = self.roadcurrentcoord + self.roaddir
-        if newcoord[0] >= self.width or newcoord[0] < 0:
-            return None
-        if newcoord[1] >= self.height or newcoord[1] < 0:
-            return None
-
-        self.roadcurrentcoord += self.roaddir
-
-        road = Road(self.next_id(), tuple(self.roadcurrentcoord), self)
-        self.grid.place_agent(road, tuple(self.roadcurrentcoord))
-
-        return road
-
-
-    def singleroad(self, initialcoord=(0,0)):
-        #initialize roads
-        print("placing road seed: ", initialcoord)
-        roaddir = self.random.randrange(4)
-        roadseedx = self.random.randrange(self.width)
-        roadseedy = self.random.randrange(self.height)
-        road = Road(self.next_id(), (roadseedx, roadseedy), self)
-        self.roadcurrentcoord = (roadseedx, roadseedy)
-        self.grid.place_agent(road, (roadseedx, roadseedy))
-
-        #note: roads are not scheduled because they do nothing
-        road = None
-        counter = 0
-        roadlist = []
-        for i in range(self.initial_roads):
-            while road is None:
-                val = self.random.random()
-                #print("val: " , val)
-                if val <= self.straightweight:
-                    road = self.roadplace_random(HoominWorld.STRAIGHT)
-                elif val > self.straightweight and val <= self.leftweight + self.straightweight:
-                    road = self.roadplace_random(HoominWorld.LEFT)
-                elif val > self.leftweight + self.straightweight:
-                    road = self.roadplace_random(HoominWorld.RIGHT)
-                if road is None:
-                    print("err: road is none")
-
-                roadlist.append(road);
-            road = None
-            counter += 1
-        print("initialized ", counter, " road tiles")
-        self.roadset = self.roadset.union(set(roadlist))
-        del roadlist
-
 
     def __init__(self, height=50, width=50, initial_hoomins=10):
         super().__init__()
@@ -219,6 +141,85 @@ class HoominWorld(Model):
         self.roadplace_grid()
         self.running = True
         self.datacollector.collect(self)
+
+    def roadplace_grid(self):
+        for h in range(self.height):
+            if h % self.gridspacing is 0:
+                for w in range(self.width):
+                    road = Road(self.next_id(), (w,h), self)
+                    self.grid.place_agent(road, (w,h))
+
+        for w in range(self.width):
+            if w % self.gridspacing is 0:
+                for h in range(self.height):
+                    road = Road(self.next_id(), (w,h), self)
+                    self.grid.place_agent(road, (w,h))
+
+
+
+
+    def roadplace_random(self, direction=0):
+
+        if direction is HoominWorld.STRAIGHT:
+            True
+        elif direction is HoominWorld.LEFT:
+            self.roaddir = np.array((-1 * self.roaddir[1], self.roaddir[0]))
+        elif direction is HoominWorld.RIGHT:
+            self.roaddir = np.array((self.roaddir[1], -1 * self.roaddir[0]))
+        else:
+            self.roaddir = np.array((0,0))
+            print("bad bad bad")
+        print("placing road, direction ", direction, " coord: ", self.roadcurrentcoord)
+
+
+        newcoord = self.roadcurrentcoord + self.roaddir
+        if newcoord[0] >= self.width or newcoord[0] < 0:
+            return None
+        if newcoord[1] >= self.height or newcoord[1] < 0:
+            return None
+
+        self.roadcurrentcoord += self.roaddir
+
+        road = Road(self.next_id(), tuple(self.roadcurrentcoord), self)
+        self.grid.place_agent(road, tuple(self.roadcurrentcoord))
+
+        return road
+
+
+    def singleroad(self, initialcoord=(0,0)):
+        #initialize roads
+        print("placing road seed: ", initialcoord)
+        roaddir = self.random.randrange(4)
+        roadseedx = self.random.randrange(self.width)
+        roadseedy = self.random.randrange(self.height)
+        road = Road(self.next_id(), (roadseedx, roadseedy), self)
+        self.roadcurrentcoord = (roadseedx, roadseedy)
+        self.grid.place_agent(road, (roadseedx, roadseedy))
+
+        #note: roads are not scheduled because they do nothing
+        road = None
+        counter = 0
+        roadlist = []
+        for i in range(self.initial_roads):
+            while road is None:
+                val = self.random.random()
+                #print("val: " , val)
+                if val <= self.straightweight:
+                    road = self.roadplace_random(HoominWorld.STRAIGHT)
+                elif val > self.straightweight and val <= self.leftweight + self.straightweight:
+                    road = self.roadplace_random(HoominWorld.LEFT)
+                elif val > self.leftweight + self.straightweight:
+                    road = self.roadplace_random(HoominWorld.RIGHT)
+                if road is None:
+                    print("err: road is none")
+
+                roadlist.append(road);
+            road = None
+            counter += 1
+        print("initialized ", counter, " road tiles")
+        self.roadset = self.roadset.union(set(roadlist))
+        del roadlist
+
 
     def get_hoomin_level(self):
         return self.hoomin_level
