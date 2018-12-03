fc8a58a97e1355247cbba4123ce4d9176d3068c4
foo <foo>
2018-12-03 16:25:18 -0600

    Initial trial run. Get ready...

--- run.py
+++ run.py
@@ -4,14 +4,30 @@ import settings
 
 
 #assumes settings from git 2b07649
-def trial_social():
+def trial_social(num_trials):
     settings.socialswitchprobability = 0.01
     settings.randomswitchprobability = 0.05
 
-    hworld = HoominWorld()
-    
+    for x in range(num_trials):
+        hworld = HoominWorld(logtag="social"+str(x))
+        hworld.run_model()
+        del hworld
+
+#assumes settings from git 2b07649
+def trial_neet(num_trials):
+    settings.socialswitchprobability = 0.00
+    settings.randomswitchprobability = 1.0
+
+    for x in range(num_trials):
+        hworld = HoominWorld(logtag="neet"+str(x))
+        hworld.run_model()
+        del hworld
+
 if settings.runheadless:
-    hworld = HoominWorld()
-    hworld.run_model()
+    print("running social model")
+    trial_social(200)
+    print("running NEET model")
+    trial_neet(200)
+    print("DONE")
 else:
     server.launch()
