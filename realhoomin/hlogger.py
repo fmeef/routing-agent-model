5b21b31cb87641da036cfd0449ffd8f963ac5bc7
foo <foo>
2018-12-03 15:14:17 -0600

    Flush logging db after write

--- realhoomin/hlogger.py
+++ realhoomin/hlogger.py
@@ -23,7 +23,6 @@ class Logging:
                 return False
             elif filename in self.files:
                 self.files[filename].close()
-
             self.files[filename] = open(p, 'w')
             return True
         else:
@@ -60,5 +59,5 @@ class Logging:
             return False
 
         self.files[filename].write(data + '\n')
-
+        self.files[filename].flush()
         return True
